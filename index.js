82d482730cbb49992d1465c165b10a7985d3a427
hasan.akpolad <hasan.akpolad@detaysoft.com>
2022-06-03 09:09:42 +0300

    fix

--- index.js
+++ index.js
@@ -4,29 +4,58 @@ import buffer from "buffer"
 
 
 var counter = 0;
-
-(async() => {
+var urls = [""];
+(async () => {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto("https://www.youtube.com/results?search_query=nutricon+colon+cancer")
-    const video = await page.$$('#dismissible');
-    if (counter !== 40) {
-        await video[counter].click({ button: "right" })
-            // var aElement = await page.$('#thumbnail')
-            // var url = await (await aElement.getProperty('href')).jsonValue()
-        var url = await page.$$eval('a', element => element.getAttribute("href"))
-
-        console.log(url);
-        const data = new Uint8Array(Buffer.from("url"))
-        fs.writeFile('url.txt', data, (err) => { if (err) throw err; })
-            // Write("a")
-        counter++;
+    const videos = await page.$$('#thumbnail');
+    
+    autoScroll(page)
+    
+    await page.evaluate(_ => {
+        window.scrollBy(0, window.innerHeight);
+    });
+    console.log(videos.length);
+    while(counter <= 40)
+    {
+        counter ++;
     }
+    // console.log(videos.length);
+    // await page.waitForTimeout(50000);
+
+    for (let video of videos) {
+        try {
+            console.log(await (await video.getProperty('href')).jsonValue());
+            urls += "\n" + await (await video.getProperty('href')).jsonValue();
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    await page.waitForTimeout(100)
+
+    const data = new Uint8Array(Buffer.from(urls))
+    fs.writeFile('url.txt', data, (err) => { if (err) throw err; })
     await page.screenshot("image.png");
     await browser.close()
 })();
 
-function Write(params) {
-    const data = new Uint8Array(Buffer.from(params))
-    fs.writeFile('url.txt', data, (err) => { if (err) throw err; })
+async function autoScroll(page) {
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight - window.innerHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
 }
